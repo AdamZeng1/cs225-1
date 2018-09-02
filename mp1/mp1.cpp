e8b5084e724119fdbc6f4124f8fab253b8fa9ac9
Marc Harvey <marcharvey1470@gmail.com>
2018-09-02 15:44:01 -0500

    finished mp1

--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -5,16 +5,39 @@ using cs225::PNG;
 using cs225::HSLAPixel;
 
 #include <string>
-
+#include <math.h> /* sin */
 
 
 void rotate(std::string inputFile, std::string outputFile) {
   // TODO: Part 2
+	PNG image;
+	image.readFromFile(inputFile);
+	unsigned int width = image.width();
+	unsigned int height = image.height();
+	PNG output(width, height);
+	for (unsigned x = 0; x < width; x++) {
+		for (unsigned y = 0; y < height; y++) {
+			HSLAPixel & inPixel = image.getPixel(x, y);
+			HSLAPixel & outPixel  = output.getPixel(width - x - 1, height - y - 1);
+			outPixel = inPixel;
+		}
+	}
+	output.writeToFile(outputFile);
 }
 
 PNG myArt(unsigned int width, unsigned int height) {
-  PNG png(width, height);
-  // TODO: Part 3
-
-  return png;
+	double DOT_SIZE = .1; /* bigger means smaller dots */
+	PNG png(width, height);
+  	// TODO: Part 3
+	for (unsigned x = 0; x < png.width(); x++) {
+		for (unsigned y = 0; y < png.height(); y++) {
+			HSLAPixel & curPixel = png.getPixel(x, y);
+			double intensity = sin(DOT_SIZE * x) * sin(DOT_SIZE * y);
+			curPixel.h = intensity * 45 + 270.0 * (double(x) / double(width)) + 45;
+			curPixel.s = 1.0;
+			curPixel.l = 0.5;
+			curPixel.a = 1.0;
+		}
+	}
+	return png;
 }
