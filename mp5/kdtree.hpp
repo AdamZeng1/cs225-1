0fcf6a2b4312eb086763b54ce7626ceb7afdf2b1
Marc Harvey <marcharvey1470@gmail.com>
2018-11-06 18:01:52 -0600

    finished mp5 + commenting

--- mp5/kdtree.hpp
+++ mp5/kdtree.hpp
@@ -13,9 +13,10 @@ template <int Dim>
 bool KDTree<Dim>::smallerDimVal(const Point<Dim>& first,
                                 const Point<Dim>& second, int curDim) const
 {
-    if(first[curDim]==second[curDim])
+	if (curDim < 0 || curDim >= Dim) return false;  // Check if curDim is valid
+    if(first[curDim]==second[curDim])  // If the point have same value at curDim, tiebreak 
       	return first<second;
-    return (first[curDim] < second[curDim]);
+    return (first[curDim] < second[curDim]);  // Tiebreak
 }
 
 template <int Dim>
@@ -24,11 +25,11 @@ bool KDTree<Dim>::shouldReplace(const Point<Dim>& target,
                                 const Point<Dim>& potential) const
 {
     double cur_dist = 0, pot_dist = 0;
-    for(int i=0; i<Dim;i++){
+    for(int i=0; i<Dim;i++){  // Add up squares of differences for each dimension
        	cur_dist += ((target[i]-currentBest[i])*(target[i]-currentBest[i]));
        	pot_dist += ((target[i]-potential[i])*(target[i]-potential[i]));
     }
-    if(cur_dist==pot_dist) return potential < currentBest;
+    if(cur_dist==pot_dist) return potential < currentBest;  // Tiebreak
     return pot_dist<cur_dist;
 }
 
@@ -60,24 +61,24 @@ template <int Dim>
 Point<Dim>& KDTree<Dim>::quickSelect(vector<Point<Dim>>& list, int dimension, unsigned left, unsigned right, unsigned k){
   	if(left==right) return list[left];
   	unsigned pivotIndex = k;
-  	pivotIndex = quickSelect_position(list,dimension,left,right,pivotIndex);
+  	pivotIndex = quickSelect_position(list,dimension,left,right,pivotIndex);  // partition
   	if(k==pivotIndex) return list[k];
-  	else if(k<pivotIndex)
+  	else if(k<pivotIndex)  // Traverse to left of pivot
     	return quickSelect(list, dimension, left, pivotIndex-1, k);
-  	else return quickSelect(list, dimension, pivotIndex+1, right, k);
+  	else return quickSelect(list, dimension, pivotIndex+1, right, k); // Traverse to right of pivot
 }
 
 template <int Dim>
 typename KDTree<Dim>::KDTreeNode* KDTree<Dim>::ctorHelper(vector<Point<Dim>>& points_, int dimension, unsigned left, unsigned right){
   	if(points_.empty()||left<0||right>=points_.size()||right<0||left>=points_.size())
-    	return NULL;
+    	return NULL;  // Check if parameters are in bounds
   	if(left>right) return NULL;
-  	unsigned median_idx = (left+right)/2;
+  	unsigned median_idx = (left+right)/2;  // Choose pivot index
   	KDTreeNode* subroot_ = new KDTreeNode(quickSelect(points_,dimension%Dim,left,right,median_idx));
   	size+=1;
   	dimension++;
-  	subroot_->left = ctorHelper(points_,dimension,left,median_idx-1);
-  	subroot_->right = ctorHelper(points_,dimension,median_idx+1,right);
+  	subroot_->left = ctorHelper(points_,dimension,left,median_idx-1);  // Recurse left side
+  	subroot_->right = ctorHelper(points_,dimension,median_idx+1,right);  // Recurse right side
  	return subroot_;
 }
 
@@ -111,44 +112,55 @@ KDTree<Dim>::~KDTree() {
 
 template <int Dim>
 Point<Dim> KDTree<Dim>::findNearestNeighbor(const Point<Dim>& query) const {
-    Point<Dim> nn = findNearestNeighbor(root, query, 0);
-    return nn;
+    return findNearestNeighbor(root, query, 0);  // Use helper function
 }
 
 template <int Dim>
 Point<Dim> KDTree<Dim>::findNearestNeighbor(KDTreeNode * subroot, const Point<Dim>& query, int dimension) const {
 	Point<Dim> currentBest = subroot->point;
 	bool flag;
+	// Check bounds
+	if (subroot->left == NULL && subroot->right == NULL) return subroot->point;
+	// Should traverse left subtree?
 	if (smallerDimVal(query, subroot->point, dimension)) {
-		if (subroot->left == NULL)
-			return subroot->point;
-		currentBest = findNearestNeighbor(subroot->left, query, (dimension + 1) % Dim);
-		flag = true;
+		if (subroot->left == NULL)  // If can't traverse left, then traverse right
+			currentBest = findNearestNeighbor(subroot->right, query, (dimension + 1) % Dim);
+		else  // Traverse left
+			currentBest = findNearestNeighbor(subroot->left, query, (dimension + 1) % Dim);
+		flag = true;  // Set left flag to true
 	}
+	// Should traverse right subtree?
 	else {
-		if (subroot->right == NULL)
-			return subroot->point;
-		currentBest = findNearestNeighbor(subroot->right, query, (dimension + 1) % Dim);
-		flag = false;
+		if (subroot->right == NULL)  // If can't traverse right, then traverse left
+			currentBest = findNearestNeighbor(subroot->left, query, (dimension + 1) % Dim);
+		else  // Traverse right
+			currentBest = findNearestNeighbor(subroot->right, query, (dimension + 1) % Dim);
+		flag = false;  // Set left flag to false
 	}
+	// If current node's point is closer to target, switch it
 	if (shouldReplace(query, currentBest, subroot->point)) currentBest = subroot->point;
 	
+	// Calculate radius of closest point and then also split distance
 	double radius = 0;
 	for (int i = 0; i < Dim; i++) {
 		radius += (query[i] - currentBest[i]) * (query[i] - currentBest[i]);
 	}
 	double split_distance = subroot->point[dimension] - query[dimension];
 	split_distance = split_distance * split_distance;
+
+	// Check if we need to traverse the other subtree or not
 	if (split_distance <= radius) {
 		KDTreeNode * need_to_check = flag ? subroot->right : subroot->left;
-		if (need_to_check != NULL) {
+		if (need_to_check != NULL) {  // If we can traverse other subtree, then traverse
 			Point<Dim> otherBest = findNearestNeighbor(need_to_check, query, (dimension + 1) % Dim);
+			// Replace currentbest if need be
 			if (shouldReplace(query, currentBest, otherBest)) currentBest = otherBest;
 		}
 	}
 	return currentBest;
 }
 
+// Helper function for destructor and = operator
 template <int Dim>
 void KDTree<Dim>::clear(KDTreeNode * subroot) {
 	if (subroot == NULL) return;
@@ -158,6 +170,7 @@ void KDTree<Dim>::clear(KDTreeNode * subroot) {
 	subroot = NULL;
 }
 
+// Helper function for copy constructor and = operator
 template <int Dim>
 void KDTree<Dim>::copy(KDTreeNode * subroot, KDTreeNode * othersubroot) {
 	subroot = new KDTreeNode();
--- mp5/maptiles.cpp
+++ mp5/maptiles.cpp
@@ -20,20 +20,23 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
 {
     vector<Point<3>> all_points;
     map<Point<3>, TileImage*> map_;
+    // Iterate through all tilse, mapping their average pixel in a new array to them
     for (auto iter = theTiles.begin(); iter != theTiles.end(); iter++) {
-    	LUVAPixel pixel_ = iter->getAverageColor();
+    	LUVAPixel pixel_ = iter->getAverageColor(); // Get average pixel in Point form
     	Point<3> point_ = convertToXYZ(pixel_);
-    	all_points.push_back(point_);
-    	map_[point_] = &*iter;
+    	all_points.push_back(point_);  // Push average pixel to back of all_points
+    	map_[point_] = &*iter;  // Map that average pixel to its tile
     }
-    KDTree<3> kdt_avgcolor(all_points);
+    KDTree<3> kdt_avgcolor(all_points);  // Make a kd tree of all average points
+    // Make a new canvas to be returned
     MosaicCanvas * canvas = new MosaicCanvas(theSource.getRows(), theSource.getColumns());
+    // Itreate through canvas, choosing a best tile based on the average color of each region
     for (int i = 0; i < canvas->getRows(); i++) {
     	for (int j = 0; j < canvas->getColumns(); j++) {
-    		Point<3> goal_ = convertToXYZ(theSource.getRegionColor(i, j));
-    		Point<3> best_ = kdt_avgcolor.findNearestNeighbor(goal_);
-    		TileImage* correct_tile = map_[best_];
-    		canvas->setTile(i, j, correct_tile);
+    		Point<3> goal_ = convertToXYZ(theSource.getRegionColor(i, j));  // Average color of region
+    		Point<3> best_ = kdt_avgcolor.findNearestNeighbor(goal_);  // Find NN of average color of region
+    		TileImage* correct_tile = map_[best_];  // Get the tile that is mapped to by best point
+    		canvas->setTile(i, j, correct_tile);  // Set that tile on the canvas
     	}
     }
     return canvas;
